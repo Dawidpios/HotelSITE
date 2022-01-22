b03f4c21a41f9517ea446f63873903e629a5fc3b
Dawidpios <95360547+Dawidpios@users.noreply.github.com>
2022-01-22 14:21:17 +0100

    Update Navi.js

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -40,7 +40,7 @@ const Navi = () => {
             <Li onClick={handleShowLogin}>Zaloguj się</Li>
           ) : (
             <LiLogin>
-              <p><Link to="/HotelSITE/myprofil">Mój profil</Link></p>
+              <p><Link to="/myprofil">Mój profil</Link></p>
               <p onClick={userLogOut}>Wyloguj</p>
             </LiLogin>
           )}
@@ -54,7 +54,7 @@ const Navi = () => {
           ) : (
             <LiLogin>
               
-              <p><Link to="/HotelSITE/myprofil">Mój profil</Link></p>
+              <p><Link to="/myprofil">Mój profil</Link></p>
               <p onClick={userLogOut}>Wyloguj</p>
             </LiLogin>
           )}
